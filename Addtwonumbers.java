ab1c23bfc27ef318a9e54277858d6fef19face45
pawanraghav640 <pawanraghav640@gmail.com>
2025-08-14 11:59:06 +0530

    Deleted Files

--- Addtwonumbers.java
+++
@@ -1,9 +0,0 @@
-public class Addtwonumbers {
-    public static void main(String[] args) {
-        int n = 5;
-        int m = 7;
-        int s = m + n;
-        System.out.println("Sum ="+s);
-    }
-
-}
--- areaofcircle.java
+++
@@ -1,9 +0,0 @@
-public class areaofcircle {
-        public static void main(String[] args) {
-        double radius = 5;
-        double area = Math.PI * radius * radius;
-        System.out.println("Area of circle = " + area);
-    }
-
-    
-}
