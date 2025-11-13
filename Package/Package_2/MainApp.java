77a9055d0ed60173ce7038ebb340d7043a4086a9
Pawan Kumar <pawanraghav640@gmail.com>
2025-11-13 21:11:20 +0530

    Delete Package directory

--- Package/Package_2/MainApp.java
+++
@@ -1,9 +0,0 @@
-package Package.Package_2;
-import Package.Package_2.com.university.department.cse.*;
-
-public class MainApp {
-	public static void main(String[] args) {
-		Course detail = new Course();
-		detail.details();
-	}
-}
