2eff666bca93f1334b584c66efe35ba3553b4a0b
Pawan Kumar <pawanraghav640@gmail.com>
2025-11-13 21:09:16 +0530

    Delete programming_elements directory

--- programming_elements/Level 1 Lab Practice/HarryAge.java
+++
@@ -1,8 +0,0 @@
-// Q1. Harry’s Age
-class HarryAge {
-    public static void main(String[] args) {
-        int birthYear = 2000, currentYear = 2024;
-        int age = currentYear - birthYear;
-        System.out.println("Harry's age in " + currentYear + " is " + age);
-    }
-}
